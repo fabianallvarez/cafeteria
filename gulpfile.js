f0260331bc00c34fc051fe386cd6f1c28f33274b
fabianallvarez <fabianallvarez@gmail.com>
2023-09-21 22:16:09 +0200

    Ft: instalando version 7.1.0 gulp imagemin. Slavando 9.61mg

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 
 // Imagenes
-
+const imagemin = require('gulp-imagemin');
 
 function css(done) {
     src('src/scss/app.scss')
@@ -19,6 +19,7 @@ function css(done) {
 
 function imagenes() {
     return src('src/img/**/*')
+        .pipe(imagemin({ optimizationlevel: 3 }))
         .pipe(dest('build/img'));
 }
 
