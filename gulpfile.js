59336dc77c00ea7927763f8335bfdf0516ac26db
fabianallvarez <fabianallvarez@gmail.com>
2023-09-23 11:38:24 +0200

    Ft: instalando dependencias Avif.

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const autoprefixer = require('autoprefixer');
 // Imagenes
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
-
+const avif = require('gulp-avif');
 function css(done) {
     src('src/scss/app.scss')
         .pipe(sass())
@@ -30,6 +30,12 @@ function versionWebp() {
         .pipe(dest('build/img'))
 }
 
+function versionAvif() {
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(avif())
+        .pipe(dest('build/img'))
+}
+
 function dev() {
     watch('src/scss/**/*.scss', css)
     watch('src/img/**/*', imagenes)
@@ -39,7 +45,8 @@ exports.css = css;
 exports.dev = dev;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.default = series(imagenes, versionWebp, css, dev);
+exports.versionAvif = versionAvif;
+exports.default = series(imagenes, versionWebp, versionAvif, css, dev);
 
 // series - Se inicia una tareas y hasta que finaliza, inicia la siguiente
 // parallel - Todas inician al mismo tiempo
